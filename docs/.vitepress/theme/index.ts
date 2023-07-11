c16db4207dfbbdfb8edef466275eb650d5f52465
guozhen <guozhen4213@126.com>
2023-07-11 11:03:29 +0800

    button

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,11 +1,14 @@
-import Theme from 'vitepress/dist/client/theme-default/index.js'
+// import Theme from 'vitepress/dist/client/theme-default/index.js'
+import Theme from 'vitepress/theme'
 import SmartyUI from '../../../src/entry'
+import 'vitepress-theme-demoblock/dist/theme/styles/index.css'
 import { useComponents } from './useComponents'
 
 export default {
   ...Theme,
-  enhanceApp({ app }) {
-    app.use(SmartyUI)
-    useComponents(app)
+  enhanceApp(ctx) {
+    Theme.enhanceApp(ctx)
+    ctx.app.use(SmartyUI)
+    useComponents(ctx.app)
   },
 }
--- docs/.vitepress/theme/useComponents.js
+++ docs/.vitepress/theme/useComponents.js
@@ -1,5 +1,4 @@
 // Don't remove this file, because it registers the demo components.
-import 'vitepress-theme-demoblock/dist/theme/styles/index.css'
 import Demo from 'vitepress-theme-demoblock/dist/client/components/Demo.vue'
 import DemoBlock from 'vitepress-theme-demoblock/dist/client/components/DemoBlock.vue'
 
